b4a2e5489ce8b62c307f5381a85f35a87bdf0569
Drish <148125191+drishmahajan@users.noreply.github.com>
2025-11-18 02:07:30 +0530

    Update static file serving path in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
